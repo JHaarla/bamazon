dd8783d3a904cd281509580c63c9bf60e98e02f8
JHaarla <haarladesign@gmail.com>
2019-10-05 01:24:30 -0400

    added seed products to DB

--- bamazon-seed.sql
+++ bamazon-seed.sql
@@ -6,7 +6,7 @@ CREATE TABLE products (
     item_id INT NOT NULL,
     product_name VARCHAR(75) NOT NULL,
     department_name VARCHAR(50) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
+    price DECIMAL(12, 2) NOT NULL,
     stock_quantity INT NOT NULL,
     PRIMARY KEY (item_id)
 );
